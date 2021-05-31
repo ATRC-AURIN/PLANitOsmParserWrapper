ed55a1a0479ee8128ef54e325b7d70fb1e8ea9ee
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2021-05-31 17:01:03 +1000

    - made a start with testing skeleton setup

---
+++ src/test/java/org/planit/aurin/test/AurinWrapperTest.java
@@ -0,0 +1,38 @@
+package org.planit.aurin.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.planit.aurin.parser.PlanitAurinParserMain;
+
+
+/**
+ * Test the aurin Wrapper
+ * 
+ * @author markr
+ *
+ */
+public class AurinWrapperTest {
+
+  /**
+   * Test with maximum number of defaults
+   */
+  @Test
+  public void defaultsTest() {
+    try {
+
+      // Run with settings
+      //PlanitAurinParserMain.main(args);
+
+      // Create stream based approach for testing
+      
+      
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Error when testing Aurin Wrapper with max defaults ");
+    }
+  }
+
+}
+
