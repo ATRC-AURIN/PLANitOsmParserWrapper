5f6a639b21785e92b95e60855f7bef2ed0982067
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2022-03-21 11:47:45 +1100

    #14
    - powershell script for test flow
    -- single line run argument required for powershell
    -- force creation of dirs otherwise error if they already exist
    -- data dir under atrc dir to keep consistent

---
+++ atrc/workflow_run_test.ps1
@@ -0,0 +1,19 @@
+# This test script simulates the steps that the
+# workflow runner will perform when running a 
+# particular operation in Windows powershell 
+
+# Build the container image: (this can be commented out for speedier test runs if the image is current)
+#docker build -t osmparserwrapper-atrc -f ./atrc/Dockerfile .
+
+# Create folders for data input and output within ATRC sub dir:
+mkdir -Force data
+mkdir -Force ./atrc/data/inputs
+mkdir -Force ./atrc/data/outputs
+
+# Copy over test parameters and files:
+cp ./atrc/test_inputs/* ./atrc/data/inputs/
+cp ./atrc/test_parameters.yaml ./atrc/data/parameters.yaml
+
+# Run the container:
+docker run --mount type=bind,source="${PWD}"/atrc/data,target=/data osmparserwrapper-atrc
+    
--- atrc/workflow_run_test.sh
+++ atrc/workflow_run_test.sh
@@ -5,7 +5,7 @@
 # particular operation. 
 
 # Build the container image: (this can be commented out for speedier test runs if the image is current)
-docker build -t osmparserwrapper -f atrc/Dockerfile .
+docker build -t osmparserwrapper-atrc -f atrc/Dockerfile .
 
 # Create folders for data input and output:
 mkdir data
@@ -19,5 +19,5 @@ cp atrc/test_parameters.yaml data/parameters.yaml
 # Run the container:
 docker run \
     --mount type=bind,source="$(pwd)"/data,target=/data \
-    osmparserwrapper
+    osmparserwrapper-atrc
     
