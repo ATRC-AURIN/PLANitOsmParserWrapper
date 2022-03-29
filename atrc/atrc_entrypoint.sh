70aff0a686cb1871e697a83b21be619cab54c0f8
MarkRaadsen <52301193+MarkRaadsen@users.noreply.github.com>
2022-03-29 17:54:18 +1100

    - add conversion to linux eol to script for parameters file - update directory structure ot be in line with shell script

--- atrc/atrc_entrypoint.sh
+++ atrc/atrc_entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# run dos2unix on the parameters script to prevent incorrect if file comes from Windows
+eval "dos2unix /atrc_data/parameters.yaml"
+
 function parse_yaml { 
   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
   sed -ne "s|^\($s\):|\1|" -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 | awk -F$fs '{indent = length($1)/2; vname[indent] = $2; for (i in vname) {if (i > indent) {delete vname[i]}} if (length($3) > 0) {vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")} printf("%s%s=\"%s\"\n", vn, $2, $3);}}'
